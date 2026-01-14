48d7fe89b51b394957d3bddbf35d0eb0f6d808b9
34j <34j.github@proton.me>
2026-01-14 21:17:26 +0900

    chore: add instructions.md

--- src/biem_helmholtz_2d/quadrature.py
+++ src/biem_helmholtz_2d/quadrature.py
@@ -1,7 +1,9 @@
 from array_api._2024_12 import Array, ArrayNamespaceFull
 
+
 def trapezoidal_quadrature(n: int, /, *, xp: ArrayNamespaceFull) -> tuple[Array, Array]:
-    r"""Trapezoidal quadrature for [0, 2π].
+    r"""
+    Trapezoidal quadrature for [0, 2π].
 
     Returns $x_j$ and $w_j$, where
 
@@ -13,21 +15,28 @@ def trapezoidal_quadrature(n: int, /, *, xp: ArrayNamespaceFull) -> tuple[Array,
     Parameters
     ----------
     n : int
-        Number of discretization points / 2
+        Harmonics which order is less than n are integrated exactly.
+    xp: ArrayNamespaceFull
+        The array namespace.
 
     Returns
     -------
     Array
         The roots $x_j$ of shape (2n - 1,).
         and weights $w_j$ of shape (2n - 1,).
+
     """
     n_quad = 2 * n - 1
     x = 2 * xp.pi * xp.arange(n_quad) / n_quad
     w = xp.full((1,), 2 * xp.pi / n_quad)
     return x, w
 
-def kussmaul_martensen_kress_quadrature(n: int, /, *, xp: ArrayNamespaceFull, x: Array | None = None) -> tuple[Array, Array]:
-    r"""Kussmaul-Martensen (Kress) quadrature.
+
+def kussmaul_martensen_kress_quadrature(
+    n: int, /, *, xp: ArrayNamespaceFull
+) -> tuple[Array, Array]:
+    r"""
+    Kussmaul-Martensen (Kress) quadrature.
 
     Returns $x_j$ and $R_j$, where
 
@@ -39,26 +48,30 @@ def kussmaul_martensen_kress_quadrature(n: int, /, *, xp: ArrayNamespaceFull, x:
     Parameters
     ----------
     n : int
-        Number of discretization points / 2
-    x : Array
-        The target points of shape (...,).
-        If None, it will be set to 0.
+        Harmonics which order is less than n are integrated exactly.
+    xp: ArrayNamespaceFull
+        The array namespace.
 
     Returns
     -------
     Array
         The roots $x_j$ of shape (2n - 1,).
         and weights $R_j$ of shape (2n - 1,).
+
     """
     x = xp.pi * xp.arange(2 * n) / n
     m = xp.arange(1, n)
-    w = - 2 * xp.pi / n * xp.sum(
+    w = -2 * xp.pi / n * xp.sum(
         1 / m * xp.cos(m * (t_[..., None, None] - x[:, None])), axis=-1
     ) - xp.pi / n**2 * xp.cos(n * (t_[..., None] - x))
     return x, w
 
-def garrick_wittich_quadrature(n: int, /, *, xp: ArrayNamespaceFull, x: Array | None = None) -> tuple[Array, Array]:
-    r"""Garrick-Wittich quadrature.
+
+def garrick_wittich_quadrature(
+    n: int, /, *, xp: ArrayNamespaceFull
+) -> tuple[Array, Array]:
+    r"""
+    Garrick-Wittich quadrature.
 
     Returns $x_j$ and $T_j (x)$, where
 
@@ -70,16 +83,16 @@ def garrick_wittich_quadrature(n: int, /, *, xp: ArrayNamespaceFull, x: Array | 
     Parameters
     ----------
     n : int
-        Number of discretization points / 2
-    x : Array
-        The target points of shape (...,).
-        If None, it will be set to 0.
+        Harmonics which order is less than n are integrated exactly.
+    xp: ArrayNamespaceFull
+        The array namespace.
 
     Returns
     -------
     Array
         The roots $x_j$ of shape (2n - 1,).
         and weights $T_j$ of shape (2n - 1,).
+
     """
     if x is None:
         t_ = xp.asarray(0.0)
@@ -87,7 +100,7 @@ def garrick_wittich_quadrature(n: int, /, *, xp: ArrayNamespaceFull, x: Array | 
         t_ = x
     x = xp.pi * xp.arange(2 * n) / n
     m = xp.arange(1, n)
-    w = - 1 / n * xp.sum(
+    w = -1 / n * xp.sum(
         m * xp.cos(m * (t_[..., None, None] - x[:, None])), axis=-1
-    ) - 1/ 2 * xp.cos(n * (t_[..., None] - x))
+    ) - 1 / 2 * xp.cos(n * (t_[..., None] - x))
     return x, w
