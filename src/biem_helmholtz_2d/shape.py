b7d7975bbf9be09564692a9da7547e00d8bfbefb
34j <34j.github@proton.me>
2026-01-18 19:01:41 +0900

    fix: shape

--- src/biem_helmholtz_2d/shape.py
+++ src/biem_helmholtz_2d/shape.py
@@ -1,57 +1,24 @@
-from abc import ABCMeta, abstractmethod
-from collections.abc import Sequence
+from typing import Protocol
 
 import attrs
 from array_api._2024_12 import Array
 from array_api_compat import array_namespace
 
 
-class Shape(metaclass=ABCMeta):
-    @abstractmethod
+class Shape(Protocol):
     def x(self, t: Array, /) -> Array:
-        pass
-
-    @abstractmethod
-    def dx(self, t: Array, /) -> Array:
-        pass
-
-    @abstractmethod
-    def ddx(self, t: Array, /) -> Array:
-        pass
-
-    @abstractmethod
-    def __eq__(self, other: object, /) -> bool:
-        pass
-
-    def jacobian(self, t: Array) -> Array:
-        xp = array_namespace(t)
-        return xp.sqrt(xp.sum(self.dx(t) ** 2, axis=-1))
-
-
-@attrs.define(frozen=True)
-class Shapes:
-    shapes: Sequence[Shape]
-
-    def x(self, t: Array, /) -> Array:
-        xp = array_namespace(t)
-        return xp.stack(
-            [shape.x(t) for shape in self.shapes],
-            axis=-2,
-        )
+        ...
 
     def dx(self, t: Array, /) -> Array:
-        xp = array_namespace(t)
-        return xp.stack(
-            [shape.dx(t) for shape in self.shapes],
-            axis=-2,
-        )
+        ...
 
     def ddx(self, t: Array, /) -> Array:
-        xp = array_namespace(t)
-        return xp.stack(
-            [shape.ddx(t) for shape in self.shapes],
-            axis=-2,
-        )
+        ...
+
+
+def jacobian(shape: Shape, t: Array, /) -> Array:
+    xp = array_namespace(t)
+    return xp.sqrt(xp.sum(shape.dx(t) ** 2, axis=-1))
 
 
 @attrs.define(frozen=True)
